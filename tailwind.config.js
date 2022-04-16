15ae09d44fd62f2f0b67d53b24e30a6ff2ae5341
Hasan <hasan.yuksel45@gmail.com>
2022-04-17 02:18:42 +0300

    Files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      family: ['DM Serif Display', 'serif'],
+    },
+    extend: {
+      colors: {
+        'background-color' : '#F6F7FA',
+        'heading-color' : '#081F32',
+        'red-color' : '#F8593B',
+      },
+      fontSize: {
+        '2xxl': '1.75rem',
+      }
+    },
+  },
+  plugins: [],
+}
